c83146a42cde4edcb73eabf01b43884c470dc27b
aslin-r <aslinjoelr123@gmail.com>
2024-04-04 11:58:17 +0530

    setuped the template,static  folder and configured the settings.py

--- clovion_backend/clovion_backend/models.py
+++ clovion_backend/clovion_backend/models.py
@@ -1,9 +1,13 @@
-# models.py
 from django.db import models
 
 class StudentMarks(models.Model):
+    # Define your model fields here
     subject1 = models.IntegerField()
     subject2 = models.IntegerField()
     subject3 = models.IntegerField()
     subject4 = models.IntegerField()
     subject5 = models.IntegerField()
+
+    class Meta:
+        # Explicitly specify the app_label for the model
+        app_label = 'clovion_backend'
